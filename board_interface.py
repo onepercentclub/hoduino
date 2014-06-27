d6c4cfc9cd708fcba79559056741100d0ff01bc4
Mark Gallop <mark@rollick.id.au>
2014-06-27 15:53:21 +0200

    wip: add start of buzzer code

--- board_interface.py
+++ board_interface.py
@@ -3,7 +3,7 @@
 import time
 import random
 from pyfirmata import Arduino, util
-from pyfirmata import SERVO as SERVO_MODE
+from pyfirmata import SERVO as SERVO_MODE, PWM as PWM_MODE, OUTPUT as OUTPUT_MODE
 from utils import Singleton
 
 UPPER_BOUND_DEGREES = 179
@@ -17,6 +17,9 @@ class PinManager():
     """
     dig_pin_output_format = 'd:{0}:o'
     dig_pin_input_format = 'd:{0}:i'
+
+    analog_pin_output_format = 'a:{0}:o'
+    analog_pin_input_format = 'a:{0}:i'
 
     digital_pins_available = []
     analog_pins_available = []
@@ -33,19 +36,28 @@ class PinManager():
         self.analog_pins_available = [x for x in range(6)]
 
     def get_digital_pin_out(self, pin_number=None):
-
-        print pin_number
         if (not pin_number or (pin_number not in self.digital_pins_available)):
             pin_number = self.digital_pins_available.pop()
 
-        print pin_number
         self.digital_pins_allocated.append(pin_number)
 
         return self.board.get_pin(self.dig_pin_output_format.format(pin_number))
 
+    def get_analog_pin_out(self, pin_number=None):
+        if (not pin_number or (pin_number not in self.analog_pins_available)):
+            pin_number = self.analog_pins_available.pop()
+
+        self.analog_pins_allocated.append(pin_number)
+
+        return self.board.get_pin(self.analog_pin_output_format.format(pin_number))
+
     def set_pin_mode(self, pin, mode):
         if mode == "Servo":
             pin.mode = SERVO_MODE
+        elif mode == "PWM":
+            pin.mode = PWM_MODE
+        elif mode == "OUTPUT":
+            pin.mode = OUTPUT_MODE
 
 class HoduinoBoardInterface():
     """
@@ -66,15 +78,47 @@ class HoduinoBoardInterface():
     def _setup(self):
         # Setup the pins
         self.pin_manager = PinManager(self.board)
+
         self.motor_pin = self.pin_manager.get_digital_pin_out(9)
         self.pin_manager.set_pin_mode(self.motor_pin, "Servo")
+
+        self.buzzer_pin = self.pin_manager.get_digital_pin_out(3)
+        self.pin_manager.set_pin_mode(self.buzzer_pin, "PWM")
 
     def _spin(self, degrees):
         # spin the weel
         self.motor_pin.write(degrees)
         time.sleep(SLEEP_TIME)
 
+    def _buzzer(self, value):
+        self.buzzer_pin.write(value)
+        time.sleep(SLEEP_TIME)
+        self.buzzer_pin.write(0.0)
+
+    def _melody(self):
+        notes = {
+            'c': 62, 
+            'd': 94, 
+            'e': 30, 
+            'f': 49, 
+            'g': 50, 
+            'a': 140, 
+            'b': 194, 
+            'C': 223
+          }
+
+        freqs = ['c', 'g', 'a', 'a', 'f', 'e']
+
+        for f in freqs:
+            self.buzzer_pin.write(notes[f]/255.0)
+            time.sleep(0.2)
+
+        self.buzzer_pin.write(0.0)
+
     def donation_reaction(self):
+        # buzzer
+        # self._melody()
+
         # spin procedure for donation reaction
         self._spin(90)
         self._spin(150)
